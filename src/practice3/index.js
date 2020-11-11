9a583bc050651e9125a5f7af985d7c13c5061c32
Yafeng Wang <yafeng.wang@wyf.local>
2020-11-11 20:21:48 +0800

    finish practice

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,7 @@
-export const getPoetry
+const fetch = require("node-fetch");
+
+export const getPoetry = async () => {
+  const data = await fetch("https://v1.jinrishici.com/all.json");
+  const { content, origin, author } = await data.json();
+  return [content, author, origin];
+};
